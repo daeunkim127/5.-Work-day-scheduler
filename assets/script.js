f4768e6fb9f8c97518f12aa1d994345cfa341989
Da Kim <daeunkim127@gmail.com>
2022-04-04 22:30:44 -0400

    past/future/present indicator added

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,18 @@
 //shows current date at the top
 var date = moment().format('MMMM Do, YYYY');
 $('#currentDay').text(date)
+let currentHour = moment().format("HH");
+console.log(currentHour)
 
+$('.time-div').each(function(){
+    var timeDiv = $(this).attr('id');
+    if (currentHour==timeDiv) {
+        $(this).addClass('present');
+    } else if (currentHour<timeDiv){
+        $(this).removeClass('present');
+        $(this).addClass('future')
+    } else if (currentHour > timeDiv){
+        $(this).removeClass('future');
+        $(this).addClass('past');
+    }
+})
